fdbeb07f474bb9b0e7671c42acfa62405d4c2740
Vishal-8155 <69633165+gokuviku@users.noreply.github.com>
2024-03-14 14:00:34 +0530

    completed

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const searchbox = document.querySelector(".search input");
 const searchbtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
 
 async function checkWeather(city) {
     try {
@@ -10,12 +12,24 @@ async function checkWeather(city) {
         if (!response.ok) {
             throw new Error('City not found');
         }
-        const data = await response.json();
+        var data = await response.json();
         console.log(data);
         document.querySelector(".city").innerHTML = data.name;
         document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
         document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
         document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+        if(data.weather[0].main=="Clouds"){
+            weatherIcon.src = "./cloud5.png"
+        }
+        else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "./cloud6.png"
+        }
+        else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "./cloud4.png"
+        }
+        document.querySelector(".weather").style.display="block";
+
     } catch (error) {
         console.error('Error fetching weather:', error.message);
     }
