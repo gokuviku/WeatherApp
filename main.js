d8114cb5696bb0996b8195b306fff820b7901e6c
Vishal-8155 <69633165+gokuviku@users.noreply.github.com>
2024-03-14 11:49:02 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const apiKey = 'e354099168d5730daa2a62a160912f99';
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchbox = document.querySelector(".search input");
+const searchbtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+    try {
+        const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+        if (!response.ok) {
+            throw new Error('City not found');
+        }
+        const data = await response.json();
+        console.log(data);
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+    } catch (error) {
+        console.error('Error fetching weather:', error.message);
+    }
+}
+
+searchbtn.addEventListener("click", () => {
+    checkWeather(searchbox.value);
+});
